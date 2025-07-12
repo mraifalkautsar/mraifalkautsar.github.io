9570db4678c79426c7adb3bb7e94ba443234ea26
Muhammad Ra'if Alkautsar <96478279+lotsadelight@users.noreply.github.com>
2025-07-12 12:49:43 +0700

    fix: workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/portofolio',
+  assetPrefix: '/portofolio/',
   eslint: {
     ignoreDuringBuilds: true,
   },
